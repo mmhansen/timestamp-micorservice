0e2b46e3980b2339b8cb126f407fe596b6961207
Michael Hansen <hansen.mmb@gmail.com>
2016-09-26 12:02:09 -0400

    V 0.1.0 working natural time parser

--- server.js
+++ server.js
@@ -1,14 +1,33 @@
 var express = require('express');
 var path = require('path');
+var moment = require('moment');
 
 var app = express()
-app.listen(3000);
-
-
-
+// serve up the static files from public
 app.use(express.static('./public'))
 
-app.get('/', function(req, res){
-  // somewhere in req I need to get their url
-  
+app.get('/:time', function(req, res){
+
+  var time = moment(req.params.time);
+  if (time.isValid()){
+      console.log(time);
+      var response = {
+        unix: time.unix(),
+        natural: time.format('MMMM Do YYYY, h:mm:ss a')
+
+      }
+  } else {
+    console.log(time);
+    var response  = {
+      unix: null,
+      natural: null
+    }
+  }
+
+  res.json(response);
+})
+
+
+var server = app.listen(process.env.PORT || 3000, function(){
+  console.log('App listening on port 3000')
 })
